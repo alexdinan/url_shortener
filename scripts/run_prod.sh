def85a465714c0a7fae7cb06b4aa29b9c496d7e7
alexdinan <alex.dinan10@gmail.com>
2025-12-27 18:25:45 +0000

    Added bash script to push to ECR

--- scripts/run_prod.sh
+++ scripts/run_prod.sh
@@ -1,12 +1,28 @@
+# go to root
+cd ..
+
+# ensure no uncommited changes
+if [[ -n "$(git status --porcelain)" ]]; then
+    echo "ERROR: Cannot deploy with uncommitted changes"
+    exit 1
+fi
+
+# use latest git commit hash as image tag
+GIT_HASH_TAG="$(git rev-parse --short HEAD)"
+
 # provision AWS resources
-cd ../infra
+cd infra
 terraform init
-terraform plan
 terraform apply -auto-approve
 
-# build backend docker image
-cd ../backend
-docker build tinyurl-app .
-docke push
+# get outputs from terraform
+AWS_REGION="$(terraform output -raw aws_region)"
+ECR_URL="$(terraform output -raw ecr_url)"
 
-# push to ECR
+cd backend
+# authenticate docker to aws cli
+aws ecr get-login-password --region $AWS_REGION | docker login --username AWS --password-stdin $ECR_URL
+# build and tag docker image
+docker build -t $ECR_URL:$GIT_HASH_TAG .
+# push image to ECR
+docker push $ECR_URL:$GIT_HASH_TAG
